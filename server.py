ed4272f44e3e055173656abb4da32400c49c32fd
Martin Vodanovic <martinevodanovic@gmail.com>
2025-05-27 10:48:14 -0400

    my portfolio website

---
+++ server.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, url_for, request,send_from_directory, redirect
+import os
+import csv
+
+
+app = Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                             'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route("/")
+def my_home():
+    return render_template('index.html')
+
+
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name )
+
+def write_to_file(data):
+    with open('database.txt', 'a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email},{subject},{message}')
+
+def write_to_csv(data):
+    with open('database.csv', 'a') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return  redirect('/thankyou.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'Something went wrong'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
